613a61367313cd7d1ae2b462e47e4f6618017dd1
Mark <mark.christiansen@nuim.ie>
2016-12-03 00:01:57 +0000

    Add basic league file

---
+++ league.py
@@ -0,0 +1,182 @@
+import csv
+import yaml
+import pandas
+
+
+# reorder columns
+def set_column_sequence(dataframe, seq, front=True):
+    '''Takes a dataframe and a subsequence of its columns,
+       returns dataframe with seq as first columns if "front" is True,
+       and seq as last columns if "front" is False.
+    '''
+    cols = seq[:] # copy so we don't mutate seq
+    for x in dataframe.columns:
+        if x not in cols:
+            if front: # we want "seq" to be in the front
+                # so append current column to the end of the list
+                cols.append(x)
+            else:
+                # we want "seq" to be last, so insert this
+                # column in the front of the new column list
+                # "cols" we are building:
+                cols.insert(0, x)
+    return dataframe[cols]
+
+
+class LeagueTable:
+    def __init__(self, team_ids, yaml_file, csv_file, name):
+        self._name = name
+        self._yaml_file = yaml_file
+        self._csv_file = csv_file
+        self._team_ids = team_ids
+
+    def create_schedule(self):
+        """ Create a schedule for the teams in the list and return it"""
+        s = []
+        ids = list(self._team_ids.keys())
+        if len(ids) % 2 == 1:
+            ids = ids + ["BYE"]
+
+        for i in range(len(ids)-1):
+
+            mid = int(len(ids) / 2)
+            l1 = ids[:mid]
+            l2 = ids[mid:]
+            l2.reverse()
+
+            # Switch sides after each week
+            if i % 2 == 1:
+                s = s + [zip(l1, l2)]
+            else:
+                s = s + [zip(l2, l1)]
+
+            ids.insert(1, ids.pop())
+        week_count = 1
+        fixtures = {}
+        for week in s:
+            match_count = 1
+            fixtures[week_count] = {}
+            for match in week:
+                fixtures[week_count][match_count] = [match[0], match[1]]
+                match_count += 1
+            week_count += 1
+        with open(self._yaml_file, "w") as file:
+            yaml.safe_dump(fixtures, file)
+
+    def play_week(self, week_number):
+        with open(self._yaml_file, "r") as file:
+            games = yaml.safe_load(file)[week_number]
+        for g in games:
+            pass
+        # Something something update scores
+
+    def change_name(self, team_id, new_name):
+        with open(self._csv_file, "r") as file:
+            data = yaml.safe_load(file)
+        pass
+
+    def update_scores(self, scores):
+        # TODO: Read in the values as some sort of structure, ignore first line and column though
+        with open(self._csv_file, "r") as csv_file:
+            reader = csv.reader(csv_file, delimiter=',')
+            count_row = 0
+            stats = {}
+            title_row = []
+            for row in reader:
+                if count_row % 2 == 0:
+                    if count_row == 0:
+                        title_row = row[1:]
+                        stats = {row[i]: [] for i in range(1, len(row))}
+                    else:
+                        for i in range(0, len(row[1:])):
+                            stats[title_row[i]].append(row[i+1])
+                count_row += 1
+        # TODO: Amend the values
+        for result_0, result_1 in scores:
+            stats["played"][stats["team id"].index(result_0[0])] = stats["played"][stats["team id"].index(result_0[0])]
+            stats["played"][stats["team id"].index(result_0[0])] = \
+                int(stats["played"][stats["team id"].index(result_0[0])]) + 1
+            stats["played"][stats["team id"].index(result_1[0])] = \
+                int(stats["played"][stats["team id"].index(result_1[0])]) + 1
+            stats["for"][stats["team id"].index(result_0[0])] = \
+                int(stats["for"][stats["team id"].index(result_0[0])]) + result_0[1]
+            stats["for"][stats["team id"].index(result_1[0])] = \
+                int(stats["for"][stats["team id"].index(result_1[0])]) + result_1[1]
+            stats["against"][stats["team id"].index(result_0[0])] = \
+                int(stats["against"][stats["team id"].index(result_0[0])]) + result_1[1]
+            stats["against"][stats["team id"].index(result_1[0])] = \
+                int(stats["against"][stats["team id"].index(result_1[0])]) + result_0[1]
+            if int(result_0[1]) > int(result_1[1]):
+                stats["wins"][stats["team id"].index(result_0[0])] = \
+                   int(stats["wins"][stats["team id"].index(result_0[0])]) + 1
+                stats["losses"][stats["team id"].index(result_1[0])] = \
+                    int(stats["losses"][stats["team id"].index(result_1[0])]) + 1
+            elif int(result_0[1]) == int(result_1[1]):
+                stats["draws"][stats["team id"].index(result_0[0])] = \
+                   int(stats["draws"][stats["team id"].index(result_0[0])]) + 1
+                stats["draws"][stats["team id"].index(result_1[0])] = \
+                    int(stats["draws"][stats["team id"].index(result_1[0])]) + 1
+            else:
+                stats["wins"][stats["team id"].index(result_1[0])] = \
+                   int(stats["wins"][stats["team id"].index(result_1[0])]) + 1
+                stats["losses"][stats["team id"].index(result_0[0])] = \
+                    int(stats["losses"][stats["team id"].index(result_0[0])]) + 1
+            stats["win%"][stats["team id"].index(result_0[0])] = \
+                ((int(stats["wins"][stats["team id"].index(result_0[0])]) +
+                 int(stats["draws"][stats["team id"].index(result_0[0])]) / 2) /
+                 int(stats["played"][stats["team id"].index(result_0[0])]))
+            stats["win%"][stats["team id"].index(result_1[0])] = \
+                ((int(stats["wins"][stats["team id"].index(result_1[0])]) +
+                 int(stats["draws"][stats["team id"].index(result_1[0])]) / 2) /
+                 int(stats["played"][stats["team id"].index(result_1[0])]))
+            stats["wins"][stats["team id"].index(result_0[0])] = int(stats["wins"][stats["team id"].index(result_0[0])])
+            stats["wins"][stats["team id"].index(result_1[0])] = int(stats["wins"][stats["team id"].index(result_1[0])])
+            stats["points difference"][stats["team id"].index(result_0[0])] = \
+                (int(stats["for"][stats["team id"].index(result_0[0])]) -
+                 int(stats["against"][stats["team id"].index(result_0[0])]))
+            stats["points difference"][stats["team id"].index(result_1[0])] = \
+                (int(stats["for"][stats["team id"].index(result_1[0])]) -
+                 int(stats["against"][stats["team id"].index(result_1[0])]))
+        stats_1 = pandas.DataFrame(stats)
+        # TODO: Sort the values
+        print(stats)
+        print("stats")
+
+        print(stats_1.sort_values(by=["win%", "points difference", "for", "wins", "team id"],
+                                  ascending=[0, 0, 0, 0, 0]))
+        print("stats")
+        cols = set_column_sequence(stats_1, ["team name", "team id", "played", "wins", "draws", "losses", "win%", "for",
+                                             "against", "points difference"])
+        print(cols)
+        with open(self._csv_file, "w") as file:
+            file.write(cols.to_csv())
+
+    def initialise_file(self):
+        with open(self._csv_file, 'w') as csv_file:
+            writer = csv.writer(csv_file, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(["position", "team id", "team name", "played", "wins", "draws", "losses",
+                            "win%", "for", "against", "points difference"])
+            position = 1
+            for row in self._team_ids:
+                writer.writerow([position, row, self._team_ids[row], "0", '0', '0', '0', '0', '0', '0', '0'])
+                position += 1
+
+    def display_file(self):
+        # Will display the file in an easy to read format (maybe csv for the moment)
+        pass
+
+a = LeagueTable({"T1": "tigers", "T2": "lions", "T3": "stuff", "T4": "guys", "T5": "gals", "T6": "dolls", "T7": "ducks",
+                 "T8": "then", "T9": "fine", "T10": "jaguars", "T11": "lakers", "T12": "fires"}, "test_league.yaml",
+                "test_csv.csv", "Div III.5")
+a.create_schedule()
+a.initialise_file()
+a.update_scores([(["T1", 20], ["T6", 19]), (["T10", 54], ["T9", 54]), (["T11", 15], ["T12", 12]),
+                 (["T3", 13], ["T4", 24]), (["T2", 17], ["T5", 28]), (["T7", 42], ["T8", 3])])
+# TODO: Create set of leagues each with 12 teams.  Sets us a schedule and holds it ready to play
+
+# TODO: Amend league files on the basis of the games
+
+# TODO: Amend stat files on the basis of the games for both teams and players
+
+# TODO: Figures out promotion and demotion
