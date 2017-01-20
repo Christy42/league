7c5089a597ab5469a3b22ce91faa9382654f082e
Mark <mark.christiansen@nuim.ie>
2017-01-20 20:13:34 +0000

    Make training players use the system variable to find all files required

--- sorting_players.py
+++ sorting_players.py
@@ -4,11 +4,11 @@ import yaml
 from training_modules import aging, training
 
 
-def train_players(training_folder, player_folder):
-    for file in os.listdir(training_folder):
-        with open(training_folder + "//" + file, "r") as train_file:
+def train_players():
+    for file in os.listdir(os.environ['FOOTBALL_HOME'] + "//players//training"):
+        with open(os.environ['FOOTBALL_HOME'] + "//players//training//" + file, "r") as train_file:
             training_mod = yaml.safe_load(train_file)
         focus = training_mod["focus"]
         route_assignment = training_mod["route assignment"]
-        training.training(player_folder + "//" + file, focus, route_assignment, "")
-        aging.aging(player_folder + "//" + file)
+        training.training(os.environ['FOOTBALL_HOME'] + "//players//" + file, focus, route_assignment, "")
+        aging.aging(os.environ['FOOTBALL_HOME'] + "//players//" + file)
