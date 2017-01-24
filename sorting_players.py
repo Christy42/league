071f613f1331c73d6ad5b3e45466fa61bc523f1f
Mark <mark.christiansen@nuim.ie>
2017-01-24 21:36:55 +0000

    Add a function to add a player to each team

--- sorting_players.py
+++ sorting_players.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+from create_team import add_player
 
 from training_modules import aging, training
 
@@ -12,3 +13,11 @@ def train_players():
         route_assignment = training_mod["route assignment"]
         training.training(os.environ['FOOTBALL_HOME'] + "//players//players//" + file, focus, route_assignment, "")
         aging.aging(os.environ['FOOTBALL_HOME'] + "//players//players//" + file)
+
+
+def new_players(week):
+    for team in os.listdir(os.environ['FOOTBALL_HOME'] + "//teams//teams"):
+        with open(os.environ['FOOTBALL_HOME'] + "//teams//scouting//scouts-" + team.replace(".yaml", "")) as scout_file:
+            scouts = yaml.safe_load(scout_file)
+        add_player(team.replace(".yaml", ""), attrib=scouts["attrib"], ideal_height=scouts["ideal_height"],
+                   ideal_weight=scouts["ideal_weight"], week=week, maximum=70)
